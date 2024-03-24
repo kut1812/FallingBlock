c1f58b635dd50be55159bcd7cd9e775fc47f5ec1
AnDev02 <an.nguyenthien281102@gmail.com>
2024-03-24 16:11:44 +0700

    m

---
+++ Classes/WorkingWithData/JsonManager.h
@@ -0,0 +1,40 @@
+#ifndef __JSON_MANAGER_H__
+#define __JSON_MANAGER_H__
+
+#include "cocos2d.h"
+#include "external/json/document.h"
+#include "external/json/writer.h"
+#include "external/json/stringbuffer.h"
+
+
+USING_NS_CC;
+
+class JSONManager {
+public:
+    struct PlayerData {
+        
+    };
+    struct BlockData {
+
+    };
+    struct X2CoinSkillData {
+
+    };
+    struct X2JumpSkillData {
+
+    };
+    struct ShieldSkillData {
+
+    };
+
+    static JSONManager* getInstance();
+    PlayerData loadJSONData(const std::string& filename);
+
+private:
+    static JSONManager* instance;
+
+    JSONManager();
+    ~JSONManager();
+};
+
+#endif // __JSON_MANAGER_H__
